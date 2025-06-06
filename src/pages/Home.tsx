bd9711fc9567ef7d0678145d4b422e6fd94d233b
Matthew Sikorski <sikomattrski@gmail.com>
2025-06-06 16:42:09 -0400

    update 2

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import '../style-sheets/Home.css'
+
+function Home() {
+
+    return (
+        <>
+            <h1>My freaking cool website</h1>
+            <p>This is a static site hosted on Varcel directly linked to my GitHub.</p>
+            <a href="https://github.com/gob1ingo/vite-react-project"><code>github.com/gob1ingo/vite-react-project</code></a>
+            <p>As time goes on, I will update this site with whatever grabs my interest.</p>
+        </>
+    )
+
+}
+
+export default Home
