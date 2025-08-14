e3ca7f5ec299e52ebd5132234c04681d7552d799
Matthew Sikorski <sikomattrski@gmail.com>
2025-08-14 16:01:20 -0400

    Name Card Update

---
+++ src/components/NameCard.tsx
@@ -0,0 +1,27 @@
+import "../style-sheets/namecard.css"
+
+function NameCard() {
+
+
+    return (
+        <div className="card">
+            <h1 className="name">
+                Matthew Sikorski 
+            </h1>
+            <h3 className="title">
+                Computer Scientist
+            </h3>
+            <p className="bio">
+                Hello! I am a Computer Scientist from Upstate New York. <br/>
+                Ever since I "borrowed" my older sister's DS, ever since I got to mess around with the Windows XP machines at my summer camp, I've been fascinated by computers. They've always been my interest. <br/>
+                <br/>
+                My experience with machines goes back my whole life, I've practically been raised with them.<br/>
+                The bulk of my experience is with the software of machines. <br/> 
+                The bulk of my studies at SUNY Oswego had me working with the Java language among others. <br/>
+            </p>
+        </div>
+    )
+
+}
+
+export default NameCard
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import "../style-sheets/home.css";
+import "../components/NameCard.tsx"
+import NameCard from "../components/NameCard.tsx";
 
 function Home() {
 
     return (
-        <>
-            <p>Welcome to my site!</p>
+        <div className="container">
+            <NameCard/>
             <a className="github-link" href="https://github.com/gob1ingo/gob1ingo.github.io"><code>github.com/gob1ingo/gob1ingo.github.io</code></a>
-            <p>As time goes on, I will update this site with whatever grabs my interest.</p>
-        </>
+        </div>
     )
 
 }
