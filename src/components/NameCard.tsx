747d11f1a2bc2dc2f75aa8a308c9787d4c45d61a
Matthew Sikorski <sikomattrski@gmail.com>
2025-08-14 16:14:55 -0400

    Glow Effect on Name Card

--- src/components/NameCard.tsx
+++ src/components/NameCard.tsx
@@ -13,7 +13,7 @@ function NameCard() {
             </h3>
             <p className="bio">
                 Hello! I am a Computer Scientist from Upstate New York. <br/>
-                Ever since I "borrowed" my older sister's DS, ever since I got to mess around with the Windows XP machines at my summer camp, I've been fascinated by computers. They've always been my interest. <br/>
+                Ever since I "borrowed" my older sister's DS, ever since I got to mess around with the Windows XP machines at my summer camp, I've been fascinated by computers. They've always been a core interest of mine. <br/>
                 <br/>
                 My experience with machines goes back my whole life, I've practically been raised with them.<br/>
                 The bulk of my experience is with the software of machines. <br/> 
