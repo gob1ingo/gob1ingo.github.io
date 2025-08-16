ad0d0d9660b044e96b937b8c2c8bd6a22ed3b566
Matthew Sikorski <sikomattrski@gmail.com>
2025-08-15 20:23:41 -0400

    Commented out problems. Will fix later.

--- src/components/Qualifications.tsx
+++ src/components/Qualifications.tsx
@@ -1,16 +1,18 @@
-import {useState} from 'react';
-import qData from "../data/qualifications.json";
-import '../style-sheets/qualifications.css';
+//import {useState} from 'react';
+//import qData from "../data/qualifications.json";
+//import '../style-sheets/qualifications.css';
 
 function Qualifications() {
 
-    const [expandedIndex, setExpandedIndex] = useState(null);
+    //const [expandedIndex, setExpandedIndex] = useState(null);
 
-    const q = qData.qualifications;
+    //const q = qData.qualifications;
 
+    /*
     const toggleExpanded = (index: any) => {
         setExpandedIndex(expandedIndex === index ? null : index );
     }
+    */
 
     // TODO: Implement this. 
     return (<></>)
