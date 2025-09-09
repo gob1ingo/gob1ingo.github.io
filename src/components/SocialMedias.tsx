97ad67f098b130eb0559cb6b2c186e162d8dcf73
Matthew Sikorski <sikomattrski@gmail.com>
2025-09-09 18:54:17 -0400

    fixed sm-buttons not condensing for mobile/slim screen widths.

--- src/components/SocialMedias.tsx
+++ src/components/SocialMedias.tsx
@@ -13,13 +13,13 @@ function SocialMedias() {
             <div
                 className="sm-list"
             >
-                <div
-                    className="sm-button"
-                >
-                    <div className="sm-icon">
-                        <a href="mailto:sikomattrski@gmail.com">
-                            <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpluspng.com%2Fimg-png%2Femail-icon-png-email-icon-image-122-2400.png&f=1&nofb=1&ipt=aa05bb81ea4533f3b1dd4707122c34f9416c55dcbc97aad041f16304034941ba" alt="Email"/>
-                        </a>
+                <div className="sm-main">
+                    <div className="sm-button">
+                        <div className="sm-icon">
+                            <a href="mailto:sikomattrski@gmail.com">
+                                <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpluspng.com%2Fimg-png%2Femail-icon-png-email-icon-image-122-2400.png&f=1&nofb=1&ipt=aa05bb81ea4533f3b1dd4707122c34f9416c55dcbc97aad041f16304034941ba" alt="Email"/>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 {medias.map((media) => (
